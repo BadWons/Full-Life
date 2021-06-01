3b4e0a5961a9d1485aaecb65a05c0a4daa874d02
BadWons <83222889+BadWons@users.noreply.github.com>
2021-06-01 19:54:03 +0200

    Update base.lua

--- base.lua
+++ base.lua
@@ -1,3 +1,14 @@
-Lien de la base resources etc......     :   https://gofile.io/d/9rdmn9
+Voici la base de Full Life que je leak BASE FULL OPTI POUR 200 joueurs !!
 
-Lien Sql  (il faut appeller la bdd sql   :      rosa          https://gofile.io/d/TtC0mo
+Faut désactiver le script ILLIES! pour que le serveur MARCHE.
+
+
+ https://gofile.io/d/dmhK8o = BASE A TELECHARGER AVEC LE CFG / et le resource!
+
+
+https://gofile.io/d/4wAsbf = SQL DE Full Life
+
+
+Base par! $ ERTYY # 0001
+
+Si VOUS AVEZ Besoin d ' aide MP MOI. >BadWons#1700
